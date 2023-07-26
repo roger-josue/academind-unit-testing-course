8b6a11d14e229244575fe3f319bd0701333a1820
roger-josue <jovz1798@gmail.com>
2023-07-26 13:38:12 -0600

    Testing against errors -Learned how to test if a function throws an error. -The message received as a throwable error can be compared to a string or regexp to test if the error thrown was the expected

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -29,3 +29,32 @@ it("should yield a correct sum if an array of numeric string values is provided"
     const expectedRes = nums.reduce((prev,curr) => +prev + +curr,0);
     expect(result).toBe(expectedRes);
 })
+
+it("should yield 0 if an empty array is provided", () => {
+    // Arrange
+    const numbers = [];
+    // Act
+    const result = add(numbers);
+    // Assert
+    expect(result).toBe(0);
+});
+
+it("should throw an error if no value is passed into the function as an argument", () => {
+    //Arrange not needed due to not sending a value as an argument
+    // Act
+    const resultOfFn = () => {
+        add();
+    }
+    //Assert
+    expect(resultOfFn).toThrow();
+})
+
+it("should throw an error if provided with multiple arguments instead of an array", () => {
+    //Arrange
+    //Act
+    const resultOfFn = () => {
+        add(1,2,3);
+    }
+    //Assert
+    expect(resultOfFn).toThrow(/is not iterable/);
+})
